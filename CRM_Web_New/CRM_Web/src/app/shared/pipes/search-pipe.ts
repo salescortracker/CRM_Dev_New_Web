db42c7e1e8f14418e2b8968f6d0ff9afd8a4d16d
Karishma0116 <karishma@cortracker360.com>
2026-06-17 17:38:42 +0530

    pipes
    pipes

--- CRM_Web_New/CRM_Web/src/app/shared/pipes/search-pipe.ts
+++ CRM_Web_New/CRM_Web/src/app/shared/pipes/search-pipe.ts
@@ -4,7 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'search',
 })
 export class SearchPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+ transform(items: any[], searchText: string): any[] {
+
+    if (!items || !searchText) {
+      return items;
+    }
+
+    searchText = searchText.toLowerCase();
+
+    return items.filter(item =>
+      Object.values(item).some(value =>
+        String(value)
+          .toLowerCase()
+          .includes(searchText)
+      )
+    );
   }
 }
